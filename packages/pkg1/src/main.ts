fdc8cd366f84b03d1332362c225004a279834db7
malaka <2670167939@qq.com>
2022-09-05 16:15:02 +0800

    vue 公共模块 实践

--- packages/pkg1/src/main.ts
+++ packages/pkg1/src/main.ts
@@ -1,6 +1,8 @@
-import {createVue} from '@malaka/core'
-
+import {createVue} from '@malaka/vue-common'
 import App from './App.vue'
 import router from './router'
+
 import './assets/main.css'
+
+
 createVue(App,router)
