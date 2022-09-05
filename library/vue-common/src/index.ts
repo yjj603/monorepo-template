65dca26b1e2f7a7c24c5c494487d504155f16a4a
malaka <2670167939@qq.com>
2022-09-05 14:06:13 +0800

    vue 改造

---
+++ library/vue-common/src/index.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'vue-global-api'
+import 'element-plus/dist/index.css'
+import './assets/main.css'
+import type {ComponentOptions} from "vue";
+import type {Router} from "vue-router";
+
+export function createVue(App:ComponentOptions,router:Router){
+    const app = createApp(App)
+    app.use(ElementPlus)
+    app.use(createPinia())
+    app.use(router)
+    app.mount('#app')
+}
+
---
+++ library/vue-common/src/utils/time.ts
@@ -0,0 +1,4 @@
+import dayjs from 'dayjs'
+export function timeFormat(){
+    return dayjs().format()
+}
