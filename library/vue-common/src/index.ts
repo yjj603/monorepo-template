b8fd53d329a1130d65a1773516284e98d5c778c7
malaka <2670167939@qq.com>
2022-09-07 11:41:24 +0800

    公共chart

--- library/vue-common/src/index.ts
+++ library/vue-common/src/index.ts
@@ -7,11 +7,15 @@ import "nprogress/nprogress.css"
 import type {ComponentOptions} from "vue";
 import type {Router} from "vue-router";
 
-export function createVue(App:ComponentOptions,router:Router){
+export function createVue(App:ComponentOptions,router:Router,...use:any[]){
     const app = createApp(App)
     app.use(ElementPlus)
     app.use(createPinia())
     app.use(router)
+    for (const useElement of use) {
+        app.use(useElement)
+
+    }
     app.mount('#app')
 }
 
--- library/vue-common/src/views/HomeView.vue
+++ library/vue-common/src/views/HomeView.vue
@@ -7,6 +7,5 @@
 <script setup lang="ts">
 import {MyTest} from "@/components";
 import {timeFormat} from '@malaka/utils-common'
-
 console.log(timeFormat())
 </script>
