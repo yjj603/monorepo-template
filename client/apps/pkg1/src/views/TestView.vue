6d96b18663b798b92524c968e5fb2ac915e9ad5d
malaka <2670167939@qq.com>
2022-09-28 17:35:43 +0800

    自动载入优化

--- client/apps/pkg1/src/views/TestView.vue
+++ client/apps/pkg1/src/views/TestView.vue
@@ -1,7 +1,23 @@
 <template>
   <div>公共组件测试</div>
   <MyTest></MyTest>
+  <div>workspace方法测试:</div>
+  <div>{{ now }}</div>
+  <div>{{ now2 }}</div>
+  <div>{{ now3 }}</div>
+  <div>{{ now4 }}</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  timeFormat,
+  timestamp,
+  dateFormat,
+  dateTimeFormat,
+} from "@malaka/utils-common";
+const now = timeFormat();
+const now2 = timestamp();
+const now3 = dateFormat();
+const now4 = dateTimeFormat();
+</script>
 <style scoped></style>
--- library/utils-common/time/index.ts
+++ library/utils-common/time/index.ts
@@ -1,3 +1,5 @@
 import dayjs from 'dayjs';
-export const timeFormat = ()=> dayjs().format()
-export const test = ()=>'hello world'
+export const timeFormat = (time = Date.now(),format:string  = '')=> dayjs(time).format(format)
+export const timestamp = (time = Date.now())=>dayjs(time).valueOf()
+export const dateFormat = (time = Date.now())=>dayjs(time).format('YYYY-MM-DD')
+export const dateTimeFormat = (time = Date.now())=>dayjs(time).format('YYYY-MM-DD HH:mm:ss')
