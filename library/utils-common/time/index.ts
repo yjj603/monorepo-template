32e738168e773940d12dbc6fab92cebaa26441dd
malaka <2670167939@qq.com>
2022-10-11 18:08:10 +0800

    笔记

--- library/utils-common/time/index.ts
+++ library/utils-common/time/index.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
-export const timeFormat = (time = Date.now(),format:string  = '')=> dayjs(time).format(format)
-export const timestamp = (time = Date.now())=>dayjs(time).valueOf()
-export const dateFormat = (time = Date.now())=>dayjs(time).format('YYYY-MM-DD')
-export const dateTimeFormat = (time = Date.now())=>dayjs(time).format('YYYY-MM-DD HH:mm:ss')
+
+export const timeFormat = (time = Date.now(), format: string = '') => dayjs(time).format(format);
+export const timestamp = (time = Date.now()) => dayjs(time).valueOf();
+export const dateFormat = (time = Date.now()) => dayjs(time).format('YYYY-MM-DD');
+export const dateTimeFormat = (time = Date.now()) => dayjs(time).format('YYYY-MM-DD HH:mm:ss');
